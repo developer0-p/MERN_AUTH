a4edf1670d3441221286a514ed9fd421d26947f4
Pablo Domínguez <pablo.developer0@gmail.com>
2023-05-28 15:32:49 +0200

    User controller functions added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,15 +3,34 @@ import asyncHandler from 'express-async-handler'
 // @route            POST /api/users/auth
 // @access           Public
 const authUser = asyncHandler(async (req, res) => {
-
-    //to test
-    res.status(401)
-    throw new Error('something went wrong')
-
-
     res.status(200).json({ message: "Auth user backend" })
+})
+// @description      Register a new user
+// @route            POST /api/users
+// @access           Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Register backend" })
+})
+// @description      Logout user
+// @route            POST /api/users/logout
+// @access           Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logout backend" })
+})
+// @description      Get user profile
+// @route            GET /api/users/profile
+// @access           Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "User profile" })
+})
+
+// @description      Update user profile
+// @route            PUT /api/users/profile
+// @access           Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update user profile" })
 })
 
 export {
-    authUser
+    authUser, registerUser, logoutUser, getUserProfile, updateUserProfile
 }
