c7f5ca78add51329960feeb7d335d515efdb4663
Pablo Domínguez <pablo.developer0@gmail.com>
2023-05-28 19:44:26 +0200

    Register User Endpoint added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,12 +4,33 @@ import User from '../models/userModel.js'
 // @route            POST /api/users/auth
 // @access           Public
 const authUser = asyncHandler(async (req, res) => {
+
     res.status(200).json({ message: "Auth user backend" })
 })
 // @description      Register a new user
 // @route            POST /api/users
 // @access           Public
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({ name, email, password })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
     res.status(200).json({ message: "Register backend" })
 })
 // @description      Logout user
