0e7b61f94e7186a838e054a62c87d5c0dd754432
Pablo Domínguez <pablo.developer0@gmail.com>
2023-05-29 22:17:22 +0200

    JWT Generated & cookie saved

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 // @description      Auth user/set token
 // @route            POST /api/users/auth
@@ -22,6 +23,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({ name, email, password })
 
     if (user) {
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
