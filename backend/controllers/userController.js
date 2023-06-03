916be3aed742c4b0e7043a13f4251307e5db5577
Pablo Domínguez <pablo.developer0@gmail.com>
2023-06-03 09:55:41 +0200

    Auth User Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,24 @@
 import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
-// @description      Auth user/set token
+// @description      Auth user/set token (Login)
 // @route            POST /api/users/auth
 // @access           Public
 const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
 
-    res.status(200).json({ message: "Auth user backend" })
+    const user = await User.findOne({ email })
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
 })
 // @description      Register a new user
 // @route            POST /api/users
