fdb49a8b339b75dcc68359caa1132d1be3b201c9
Pablo Domínguez <pablo.developer0@gmail.com>
2023-07-06 07:49:37 +0200

    Header Component && HomeScreen & Hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-      <h1>MERN Auth</h1>
+      <Header />
+      <HomeScreen />
     </>
   )
 }
