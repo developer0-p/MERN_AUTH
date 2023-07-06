69eccaf67384447dd08b8f39fc3c01beab35a0d5
Pablo Domínguez <pablo.developer0@gmail.com>
2023-07-06 09:30:57 +0200

    Login & Register Form UIs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       {/* Every route here */}
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 )
